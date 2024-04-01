f17148f575767fb83ef8f66324d5c7d2a507c785
akshato7 <akshathg7@gmail.com>
2024-04-01 18:59:45 +0530

    backend post req fixed

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,8 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { decode, sign, verify } from 'hono/jwt'
-import { signinInput } from "@100xdevs/medium-common";
+import { signupInput, signinInput } from "@100xdevs/medium-common";
+
 export const userRouter = new Hono<{
 	Bindings: {
 		DATABASE_URL: string;
@@ -12,12 +13,13 @@ export const userRouter = new Hono<{
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhZTljMTE1LTdmNDMtNGY5OS04YmExLTAyZjNjOTJmOTk3MSJ9.oLeaN0oZWIN0G0D4OXIyNycN9CauQP-qOR6s2Zkkuy8
 userRouter.post("/signup", async (c) => {
 	const body = await c.req.json();
-	const { success } = signinInput.safeParse(body);
-	if(!success){
-		return c.json({
-			message: "Inputs not correct";
-		})
-	}
+	// const { success } = signupInput.safeParse(body);
+	// if(!success){
+	// 	c.status(411);
+	// 	return c.json({
+	// 		message: "Inputs not good"
+	// 	})
+	// }
 	const prisma = new PrismaClient({
 		datasourceUrl: c.env?.DATABASE_URL,
 	}).$extends(withAccelerate());
