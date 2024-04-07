61c60a9cd9cc37567552d7610b77f8d2f83bc4ff
akshato7 <akshathg7@gmail.com>
2024-04-08 01:03:46 +0530

    created landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,15 @@ import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
 import { Blogs } from "./pages/Blogs";
 import { Publish } from './pages/Publish';
-
+import {Landing } from './pages/Landing'
 function App() {
 
   return (
     <>
       <BrowserRouter>
         <Routes>
+
+          <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,13 +4,13 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                    "Writing is thinking on paper. Anyone who thinks clearly can write clearly, about anything at all"
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
+                Seth Godin
                 </div>
                 <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
+                F.X. Nine
                 </div>
             </div>
         </div>
