a62898e41de8f8fc6cac8074e3236823f72f2e85
akshato7 <akshathg7@gmail.com>
2024-12-24 19:27:25 +0530

    build errors resolved

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,19 +1,19 @@
 import HeroSection from "../components/HeroSection";
-import FeaturesGrid from "../components/FeaturesGrid";
+import FeaturesGrid from "../components/FeatureGrid";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 // import { FloatingDockDemo } from "../components/Dock";
 
 export const Landing = () => {
-	return (
-		<div className="flex flex-col min-h-[100dvh]">
-			<main className="flex-1">
-				<Navbar />
-				<HeroSection />
-				<FeaturesGrid />
-				{/* <FloatingDockDemo /> */}
-				<Footer />
-			</main>
-		</div>
-	);
+  return (
+    <div className="flex flex-col min-h-[100dvh]">
+      <main className="flex-1">
+        <Navbar />
+        <HeroSection />
+        <FeaturesGrid />
+        {/* <FloatingDockDemo /> */}
+        <Footer />
+      </main>
+    </div>
+  );
 };
